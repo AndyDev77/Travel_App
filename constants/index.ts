0da2239ff93362a6e83eabb81aca947a5033072c
AndyDev77 <andyvespuce77550@gmail.com>
2023-12-05 20:36:48 +0100

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,85 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: "/", key: "home", label: "Accueil" },
+  { href: "/", key: "how_hilink_work", label: "Fonctionnement" },
+  { href: "/", key: "services", label: "Services" },
+  { href: "/", key: "pricing ", label: "Tarifs " },
+  { href: "/", key: "contact_us", label: "Contactez-nous" },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  "/person-1.png",
+  "/person-2.png",
+  "/person-3.png",
+  "/person-4.png",
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: "Les cartes réelles peuvent être hors ligne",
+    icon: "/map.svg",
+    variant: "green",
+    description:
+      "Nous vous proposons une solution qui vous permet d'utiliser notre application lorsque vous grimpez, oui des cartes hors ligne que vous pouvez utiliser à tout moment lorsqu'il n'y a pas de signal à l'endroit où vous vous trouvez.",
+  },
+  {
+    title: "Établir un programme d'aventures",
+    icon: "/calendar.svg",
+    variant: "green",
+    description:
+      "Planifiez une aventure avec vos amis. Pendant les vacances, Hilink propose de nombreuses offres intéressantes. Ainsi, il n'y a plus de discussion",
+  },
+  {
+    title: "Technologie utilisant la réalité augmentée",
+    icon: "/tech.svg",
+    variant: "green",
+    description:
+      "La technologie utilise la réalité augmentée comme guide pour votre randonnée en forêt jusqu'au sommet de la montagne. Déjà pris en charge par les dernières technologies sans connexion internet",
+  },
+  {
+    title: "De nombreux nouveaux sites chaque mois",
+    icon: "/location.svg",
+    variant: "orange",
+    description:
+      "De nombreux nouveaux sites sont proposés chaque mois, car nous disposons d'une communauté mondiale de grimpeurs qui partagent leurs meilleures expériences en matière d'escalade.",
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: "En savoir plus",
+    links: [
+      "A propos d'Hilink",
+      "Communiqués de presse",
+      "Environnement",
+      "Emplois",
+      "Contactez-nous"
+    ],
+  },
+  {
+    title: "Notre communauté",
+    links: ["Escalade xixixi", "Randonnée hilink", "Hilink kinthill"],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: "Nous contacter",
+  links: [
+    { label: "Agent administratif :", value: "123-456-7890" },
+    { label: "Responsable du courrier électronique :", value: "hilink@akinthil.com" },
+  ],
+};
+
+export const SOCIALS = {
+  title: "Social",
+  links: [
+    "/facebook.svg",
+    "/instagram.svg",
+    "/twitter.svg",
+    "/youtube.svg",
+    "/wordpress.svg",
+  ],
+};
